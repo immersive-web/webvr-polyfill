f209025b80ec29140a1e47a51b59948f5fe5dd09
Christopher Van <cvan@mozilla.com>
2016-03-23 23:04:12 -0700

    set default `WebVRConfig` options in `src/main` and extend them based on user-defined `window.WebVRConfig` (fixes #49)

--- src/util.js
+++ src/util.js
@@ -184,5 +184,25 @@ Util.isMobile = function() {
   return check;
 };
 
+// Adapted from https://mdn.io/Object/assign#Polyfill
+Util.extend = (function () {
+  if (typeof Object.assign === 'function') return Object.assign;
+  return function (target) {
+    if (target === undefined || target === null) {
+      throw new TypeError('Cannot convert undefined or null to object');
+    }
+    var output = Object(target);
+    for (var i = 1; i < arguments.length; i++) {
+      var src = arguments[i];
+      if (src === undefined || src === null) continue;
+      for (var nextKey in src) {
+        if (src.hasOwnProperty(nextKey)) {
+          output[nextKey] = src[nextKey];
+        }
+      }
+    }
+    return output;
+  };
+})();
 
 module.exports = Util;
