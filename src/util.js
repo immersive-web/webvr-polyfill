9e5f696210283201207cad2a578ee558c4f25f2c
Christopher Van <cvan@mozilla.com>
2016-04-05 16:32:19 -0700

    use third-party `Object.assign` polyfill instead

--- src/util.js
+++ src/util.js
@@ -12,6 +12,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+var objectAssign = require('object-assign');
+
 var Util = window.Util || {};
 
 Util.MIN_TIMESTEP = 0.001;
@@ -184,25 +187,6 @@ Util.isMobile = function() {
   return check;
 };
 
-// Adapted from https://mdn.io/Object/assign#Polyfill
-Util.extend = (function () {
-  if (typeof Object.assign === 'function') return Object.assign;
-  return function (target) {
-    if (target === undefined || target === null) {
-      throw new TypeError('Cannot convert undefined or null to object');
-    }
-    var output = Object(target);
-    for (var i = 1; i < arguments.length; i++) {
-      var src = arguments[i];
-      if (src === undefined || src === null) continue;
-      for (var nextKey in src) {
-        if (src.hasOwnProperty(nextKey)) {
-          output[nextKey] = src[nextKey];
-        }
-      }
-    }
-    return output;
-  };
-})();
+Util.extend = objectAssign;
 
 module.exports = Util;
