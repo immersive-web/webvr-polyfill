407c6830a6bec7a0e8106e079dda9c5da7147896
Boris Smus <boris@smus.com>
2016-05-17 12:19:56 -0700

    iOS hacking.

--- src/base.js
+++ src/base.js
@@ -82,6 +82,7 @@ VRDisplay.prototype.wrapForFullscreen = function(element) {
       'z-index: 999999 !important',
       'position: fixed',
     ];
+    this.fullscreenWrapper_.setAttribute('style', '');
     this.fullscreenWrapper_.setAttribute('style', cssProperties.join('; ') + ';');
     this.fullscreenWrapper_.classList.add('webvr-polyfill-fullscreen-wrapper');
   }
