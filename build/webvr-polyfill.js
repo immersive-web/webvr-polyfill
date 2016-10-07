236166d77bfb0d92fd16676df420d7243b0b99bc
Boris Smus <boris@smus.com>
2016-10-07 11:21:45 -0700

    saving previous timestamp

--- build/webvr-polyfill.js
+++ build/webvr-polyfill.js
@@ -4926,6 +4926,7 @@ PosePredictor.prototype.getPrediction = function(currentQ, gyro, timestampS) {
   this.outQ.multiply(this.deltaQ);
 
   this.previousQ.copy(currentQ);
+  this.previousTimestampS = timestampS;
 
   return this.outQ;
 };
