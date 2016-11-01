fed5a15ea30008f724e9009e28be25fc04b01f22
Luis Rodrigues <luis@imperodesign.com>
2016-11-01 10:47:58 +0000

    Fixed an assignment to an undeclared variable which caused errors in strict mode

--- src/util.js
+++ src/util.js
@@ -313,7 +313,7 @@ Util.frameDataFromPose = (function() {
     return out;
   };
 
-  mat4_invert = function(out, a) {
+  function mat4_invert (out, a) {
     var a00 = a[0], a01 = a[1], a02 = a[2], a03 = a[3],
         a10 = a[4], a11 = a[5], a12 = a[6], a13 = a[7],
         a20 = a[8], a21 = a[9], a22 = a[10], a23 = a[11],
